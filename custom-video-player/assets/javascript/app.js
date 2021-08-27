6f6c1c2a895f2963533627f4db56649d458299f9
Duude92 <switch4acc@gmail.com>
2021-08-27 22:24:26 +0300

    feat: added cards under player, hidden json video-array serializer

--- custom-video-player/assets/javascript/app.js
+++ custom-video-player/assets/javascript/app.js
@@ -10,12 +10,15 @@ const videoPlayer = document.querySelector('.video-player')
 const muteButton = document.querySelector('.mute')
 const nextButton = document.querySelector('.next')
 const prevButton = document.querySelector('.previous')
+const selectFilesButton = document.querySelector('.select-files')
 
 let playlist = []
 let videoTime = 0
 let isProgressBarTouching = false
 let currentVideo
 let lastVolume = 100
+let fileNameList = []
+let cards = []
 class VideoCard {
     constructor(videoName) {
         this.videoUrl = `assets/video/${videoName}.mp4`
@@ -31,11 +34,36 @@ class VideoCard {
         playVideo()
     }
     setVideo = _ => {
+        document.documentElement.style.setProperty('--progress-position', '0%')
         currentVideo = this
         video.src = this.videoUrl
         return this
     }
 
+}
+selectFilesButton.addEventListener('click', _ => {
+    document.getElementById('file-input').click()
+})
+const selectFiles = (data) => {
+    const files = Array.from(data)
+
+    files.forEach(file => {
+        let name = file.name
+        name = name.substring(0, name.lastIndexOf('.'))
+        fileNameList.push(name)
+    })
+    let serializedArray = JSON.stringify(fileNameList)
+    const type = "text/plain";
+
+    // create file
+    const a = document.createElement("a");
+    const file = new Blob([serializedArray], { type: type });
+    a.href = URL.createObjectURL(file);
+    a.download = "db.json";
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+    console.log(serializedArray)
 }
 muteButton.addEventListener('click', _ => {
     if (video.muted) {
@@ -95,7 +123,6 @@ const setVolume = (value) => {
 }
 const setVideoProgress = (value) => {
     document.documentElement.style.setProperty('--progress-position', (value / video.duration * 100) + '%')
-
 }
 
 const setVideoTime = (value) => {
@@ -153,32 +180,83 @@ const previousVideo = _ => {
     }
 }
 
-addCard(new VideoCard('Ocean').setVideo())
-addCard(new VideoCard('Rock_Balance'))
-addCard(new VideoCard('Fire'))
-addCard(new VideoCard('Ducks'))
+
+
+let xmlObj = new XMLHttpRequest()
+xmlObj.overrideMimeType("application/json");
+xmlObj.open('GET', 'assets/json/db.json', true);
+xmlObj.onreadystatechange = _ => {
+    if (xmlObj.readyState == 4 && xmlObj.status == "200") {
+        fileNameList = JSON.parse(xmlObj.responseText)
+
+        if (fileNameList.length > 0) {
+            fileNameList.forEach(fileName => {
+                let card = new VideoCard(fileName)
+                cards.push(card)
+                console.log(card)
+
+            })
+        }
+
+        //hardcode
+        console.log(cards)
+        playlist.push(cards[0])
+        playlist.push(cards[1])
+        playlist.push(cards[2])
+        playlist.push(cards[3])
+        playlist[0].setVideo()
+
+        playlist.forEach(card => {
+            let cardImage = new Image(200, 100)
+            cardImage.src = card.imageUrl
+            let container = document.createElement('div')
+            container.className = 'card-video'
+            videoSelector.appendChild(container)
+
+
+            let p = document.createElement('p')
+            p.innerText = card.title
+            container.appendChild(cardImage)
+            container.appendChild(p)
+            let selector = document.createElement('div')
+            selector.className = 'video-card-selector'
+            container.appendChild(selector)
+
+            container.addEventListener('click', _ => card.playVideo())
+        })
+        let videosContainer = document.querySelector('.videos-container')
+
+        cards.forEach(card => {
+            let cardContainer = document.createElement('div')
+            let imageContainer = new Image(400, 250)
+            imageContainer.src = card.imageUrl
+            cardContainer.append(imageContainer)
+            let p = document.createElement('p')
+            p.innerText = card.title
+            cardContainer.append(p)
+            let selector = document.createElement('div')
+            selector.className = 'video-card-selector'
+            cardContainer.appendChild(selector)
+            cardContainer.className = 'container-card-video'
+
+            videosContainer.append(cardContainer)
+        })
+
+    }
+}
+xmlObj.send(null)
+
+
+
+// addCard(new VideoCard('Ocean').setVideo())
+// addCard(new VideoCard('Rock_Balance'))
+// addCard(new VideoCard('Fire'))
+// addCard(new VideoCard('Ducks'))
 
 //Комментарий к вопросу о карточках
 console.log("https://discord.com/channels/516715744646660106/861528325087035422/880751260838535179")
 
-playlist.forEach(card => {
-    let cardImage = new Image(200, 100)
-    cardImage.src = card.imageUrl
-    let container = document.createElement('div')
-    container.className = 'card-video'
-    videoSelector.appendChild(container)
 
-
-    let p = document.createElement('p')
-    p.innerText = card.title
-    container.appendChild(cardImage)
-    container.appendChild(p)
-    let selector = document.createElement('div')
-    selector.className = 'video-card-selector'
-    container.appendChild(selector)
-
-    container.addEventListener('click', _ => card.playVideo())
-})
 
 playButtons.forEach(button => {
     button.addEventListener('click', _ => {
@@ -206,7 +284,14 @@ video.onended = _ => {
 video.ontimeupdate = (ev) => {
     if (!isProgressBarTouching) {
         let currentTime = video.currentTime
+        console.log(video.currentTime, video.duration)
+
+        if (isNaN(video.duration)) {
+            document.documentElement.style.setProperty('--progress-position', '0%')
+            return
+        }
         progressBar.value = currentTime
         document.documentElement.style.setProperty('--progress-position', (currentTime / video.duration * 100) + '%')
+        console.log(1)
     }
 }
