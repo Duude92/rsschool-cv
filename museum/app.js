3fd18ca20f3713fde1039c9cf6c8606e17c4494a
Duude92 <switch4acc@gmail.com>
2021-09-23 18:01:29 +0300

    feat: added slides

--- museum/app.js
+++ museum/app.js
@@ -1,5 +1,28 @@
-window.onload = () =>{
+class slide {
+    constructor(url) {
+        this.url = url
+    }
+    InitOnPage = (selector) => {
+        let container = document.createElement('div')
+        container.className = 'slide'
+        container.style.backgroundImage = `url(${this.url})`
+        selector.appendChild(container)
+        container.style.width = container.offsetWidth + 1000;
+    }
+}
+let sliderContainer = document.querySelector(".slides")
+
+slides = []
+slides.push(new slide('assets/img/Welcome-slider/1.jpg'))
+slides.push(new slide('assets/img/Welcome-slider/2.jpg'))
+slides.push(new slide('assets/img/Welcome-slider/3.jpg'))
+slides.push(new slide('assets/img/Welcome-slider/4.jpg'))
+slides.push(new slide('assets/img/Welcome-slider/5.jpg'))
+
+slides.forEach(slide => slide.InitOnPage(sliderContainer))
+    //---events
+window.onload = () => {
     let body = document.querySelector('.preload')
-    
-    body.className =''
-  }
+
+    body.className = ''
+}
