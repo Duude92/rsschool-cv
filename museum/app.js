77a722ef438d831827627d605229a1116e6603ca
Duude92 <switch4acc@gmail.com>
2021-09-25 19:02:48 +0300

    feat: gallery

--- museum/app.js
+++ museum/app.js
@@ -27,11 +27,12 @@ let currentSlide = document.querySelector(".actual-number")
 let videoSliderContainer = document.querySelector(".video-slider-container")
 let videoButtonLeft = document.getElementById("video-left")
 let videoButtonRight = document.getElementById("video-right")
+let galleryContainer = document.querySelector(".gallery-container")
 let sliding = false
 let isVideoSliding = false
 let slideNum = 0
 
-slides = []
+let slides = []
 slides.push(new slide('assets/img/Welcome-slider/1.jpg'))
 slides.push(new slide('assets/img/Welcome-slider/2.jpg'))
 slides.push(new slide('assets/img/Welcome-slider/3.jpg'))
@@ -40,7 +41,7 @@ slides.push(new slide('assets/img/Welcome-slider/5.jpg'))
 
 slides.forEach(slide => slide.InitOnPage(sliderContainer))
 
-videos = []
+let videos = []
 videos.push(new video('https://www.youtube.com/embed/zp1BXPX8jcU'))
 videos.push(new video('https://www.youtube.com/embed/Vi5D6FKhRmo'))
 videos.push(new video('https://www.youtube.com/embed/NOhDysLnTvY'))
@@ -48,6 +49,30 @@ videos.push(new video('https://www.youtube.com/embed/aWmJ5DgyWPI'))
 videos.push(new video('https://www.youtube.com/embed/2OR0OCr6uRE'))
 videoSliderContainer.style.width = 0
 videos.forEach(v => v.Init(videoSliderContainer))
+
+let galleryImages = []
+for (let index = 1; index < 15; index++) {
+    galleryImages.push(`assets/img/Gallery/galery${index}.jpg`)
+}
+galleryImages.sort(() => .5 - Math.random())
+for (let index = 0; index < 3; index++) {
+    const column = document.createElement('div')
+    column.className = 'column'
+    galleryContainer.append(column)
+
+    for (let index2 = 0; index2 < 5; index2++) {
+        let tempImage = new Image()
+        tempImage.src = galleryImages[index * 5 + index2]
+        column.append(tempImage)
+
+    }
+
+}
+// galleryImages.forEach(img => {
+//     let tempImage = new Image()
+//     tempImage.src = img
+//     galleryContainer.append(tempImage)
+// })
 
 //---methods
 let moveSlide = (direction) => {
