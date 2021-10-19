9fefb07f0c4ec00e39928b336bb80af18e3393d0
Duude92 <switch4acc@gmail.com>
2021-10-19 09:56:57 +0300

    feat: added localstorage item feature

--- assets/javascript/greeting.js
+++ assets/javascript/greeting.js
@@ -1,7 +1,12 @@
 import getTimeOfDay from "./timeofday.js"
+const nameContainer = document.getElementById("name")
 const greetingContainer = document.getElementById("greeting")
 let InitializeGreetingModule = _ => {
     greetingContainer.textContent = `Good ${getTimeOfDay()},`
-    console.log(getTimeOfDay())
+    nameContainer.value = localStorage.getItem('name')
 }
+window.addEventListener('beforeunload', _ => {
+    localStorage.setItem('name', nameContainer.value)
+})
+
 export default InitializeGreetingModule
