1ddeb58358d90d20eeb82ee0d142c169df316c35
Duude92 <switch4acc@gmail.com>
2021-10-19 22:16:47 +0300

    feat: added audio module

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,7 @@ import InitializeDateTime from "./datetime.js"
 import InitializeGreetingModule from "./greeting.js"
 import InitializeWeatherModule from "./weather.js"
 import "./quotes.js"
+import "./audio.js"
 
 InitializeBackgroundSlider()
 InitializeDateTime()
---
+++ assets/javascript/audio.js
@@ -0,0 +1,28 @@
+const audioPlayer = document.getElementById("audioPlayer")
+const playList = document.querySelector(".play-list")
+const playButton = document.querySelector(".play")
+const nextButton = document.querySelector(".play-next")
+const prevButton = document.querySelector(".play-prev")
+let music = []
+
+let getMusic = async _ => {
+    const result = await fetch("/assets/json/audio.json")
+    music = await result.json()
+    console.log(music)
+    music.forEach(e => {
+        const li = document.createElement('li')
+        li.textContent = e.name
+        li.className = "play-item"
+        li.onclick = _ => play(e.source)
+        playList.append(li)
+    })
+}
+let play = src => {
+    audioPlayer.src = src
+    audioPlayer.onloadedmetadata = _ => audioPlayer.play()
+}
+
+window.onload = _ => {
+    getMusic().then(_ => play(music[0].source))
+
+}
