666e697e511804f6e8e945613a862d40a9c2534c
Duude92 <switch4acc@gmail.com>
2021-10-25 14:55:54 +0300

    feat: added songs, added player behaviour

--- assets/javascript/audio.js
+++ assets/javascript/audio.js
@@ -4,6 +4,7 @@ const playButton = document.querySelector(".play")
 const nextButton = document.querySelector(".play-next")
 const prevButton = document.querySelector(".play-prev")
 let music = []
+let currentSong = 0
 
 let getMusic = async _ => {
     const result = await fetch("/assets/json/audio.json")
@@ -18,11 +19,44 @@ let getMusic = async _ => {
     })
 }
 let play = src => {
+    playButton.classList.remove('play')
+    playButton.classList.add('pause')
+
     audioPlayer.src = src
+        // audioPlayer.muted = true
     audioPlayer.onloadedmetadata = _ => audioPlayer.play()
+}
+playButton.onclick = _ => {
+    if (audioPlayer.paused) {
+        playButton.classList.remove('play')
+        playButton.classList.add('pause')
+        audioPlayer.play()
+    } else {
+        playButton.classList.add('play')
+        playButton.classList.remove('pause')
+
+        audioPlayer.pause()
+    }
+}
+nextButton.onclick = _ => {
+    if (currentSong + 1 < music.length) {
+        currentSong++
+    } else {
+        currentSong = 0
+    }
+    play(music[currentSong].source)
+
+}
+prevButton.onclick = _ => {
+    if (currentSong - 1 >= 0) {
+        currentSong--
+    } else {
+        currentSong = music.length - 1
+    }
+    play(music[currentSong].source)
+
 }
 
 window.addEventListener("load", _ => {
-    getMusic().then(_ => play(music[0].source))
-
+    getMusic().then(_ => audioPlayer.src = music[0].source)
 })
