0787088ecde104aa3a1abcd9c8c6db57a990e16b
Duude92 <switch4acc@gmail.com>
2021-10-25 15:03:11 +0300

    fix: randomized random quote

--- assets/javascript/quotes.js
+++ assets/javascript/quotes.js
@@ -3,15 +3,25 @@ const authorContainer = document.querySelector('.author')
 const changeQuote = document.querySelector('.change-quote')
 
 let quotes = []
+let lastQuoteNum = -1
 
 let getQuotes = async _ => {
     const result = await fetch("/assets/json/quotes.json")
     quotes = await result.json()
 }
 let getRandomQuote = _ => {
-    const newQuote = quotes[Math.floor(Math.random() * quotes.length)]
+    const newQuote = quotes[getDifferentRandomNumber(lastQuoteNum)]
     quoteContainer.textContent = newQuote.text
     authorContainer.textContent = newQuote.author
+}
+let getDifferentRandomNumber = lastNumber => {
+    let num = Math.floor(Math.random() * quotes.length)
+    if (num === lastNumber) {
+        return getDifferentRandomNumber(lastNumber)
+    } else {
+        lastQuoteNum = num
+        return num
+    }
 }
 
 changeQuote.onclick = _ => getRandomQuote()
